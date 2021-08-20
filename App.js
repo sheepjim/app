23b8a595a9b5d990bf284966d966c44e3f9ca5c5
sheepjim <55178385+sheepjim@users.noreply.github.com>
2021-08-20 19:52:17 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Facebook from './components/Facebook';
+
+function App() {
+  return (
+    <div className="App">
+        <Facebook />
+    </div>
+  );
+}
+
+export default App;
